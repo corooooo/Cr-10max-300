c9055e10fa9469ec709e881e6443bf20347d6596
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2025-12-18 01:27:57 +0100

    fixup

--- Marlin/src/feature/bedlevel/mbl/mesh_bed_leveling.cpp
+++ Marlin/src/feature/bedlevel/mbl/mesh_bed_leveling.cpp
@@ -68,12 +68,11 @@
       segments = ecel - scel;
       NOLESS(segments, 1);
       
-      segment_fr = (parser.inverse_time_enabled && parser.apply_feedrate_mode) ? scaled_fr_mm_s * segments : scaled_fr_mm_s;
 
       // Start and end in the same cell? No split needed.
       if (scel == ecel) {
         current_position = destination;
-        line_to_current_position(segment_fr);
+        line_to_current_position(scaled_fr_mm_s);
         return;
       }
 
