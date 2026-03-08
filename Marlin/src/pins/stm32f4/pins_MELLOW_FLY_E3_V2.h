MarlinFirmware/Update-default-Junction-Deviation-numbers-to-match-Classic-Jerk-values

♻️ Rework Multi Volume - Pins

Marlin/src/pins/stm32f4/pins_MELLOW_FLY_E3_V2.h
@@ -214,8 +214,8 @@
 
 #endif // HAS_TMC_UART
 
-#ifndef SDCARD_CONNECTION
-  #define SDCARD_CONNECTION              ONBOARD
+#ifndef VOLUME0
+  #define VOLUME0                        ONBOARD
 #endif
 
 /**
@@ -251,15 +251,15 @@
 // Onboard SD card
 // Must use soft SPI because Marlin's default hardware SPI is tied to LCD's EXP2
 //
-#if SD_CONNECTION_IS(LCD)
+#if ANY_VOLUME_IS(LCD)
   #define SD_SS_PIN                  EXP2_04_PIN
   #define SD_SCK_PIN                 EXP2_02_PIN
   #define SD_MISO_PIN                EXP2_01_PIN
   #define SD_MOSI_PIN                EXP2_06_PIN
   #define SD_DETECT_PIN              EXP2_07_PIN
-#elif SD_CONNECTION_IS(ONBOARD)
+#elif ANY_VOLUME_IS(ONBOARD)
   #define ONBOARD_SDIO                            // Use SDIO for onboard SD
-#elif SD_CONNECTION_IS(CUSTOM_CABLE)
+#elif ANY_VOLUME_IS(CUSTOM_CABLE)
   #error "No custom SD drive cable defined for this board."
 #endif
 

corooooo/Cr-10max-300

Marlin/src/pins/stm32f4/pins_MELLOW_FLY_E3_V2.h
@@ -204,8 +204,8 @@
 
 #endif // HAS_TMC_UART
 
-#ifndef SDCARD_CONNECTION
-  #define SDCARD_CONNECTION              ONBOARD
+#ifndef VOLUME0
+  #define VOLUME0                        ONBOARD
 #endif
 
 /**
@@ -241,15 +241,15 @@
 // Onboard SD card
 // Must use soft SPI because Marlin's default hardware SPI is tied to LCD's EXP2
 //
-#if SD_CONNECTION_IS(LCD)
+#if ANY_VOLUME_IS(LCD)
   #define SD_SS_PIN                  EXP2_04_PIN
   #define SD_SCK_PIN                 EXP2_02_PIN
   #define SD_MISO_PIN                EXP2_01_PIN
   #define SD_MOSI_PIN                EXP2_06_PIN
   #define SD_DETECT_PIN              EXP2_07_PIN
-#elif SD_CONNECTION_IS(ONBOARD)
+#elif ANY_VOLUME_IS(ONBOARD)
   #define ONBOARD_SDIO                            // Use SDIO for onboard SD
-#elif SD_CONNECTION_IS(CUSTOM_CABLE)
+#elif ANY_VOLUME_IS(CUSTOM)
   #error "No custom SD drive cable defined for this board."
 #endif
 